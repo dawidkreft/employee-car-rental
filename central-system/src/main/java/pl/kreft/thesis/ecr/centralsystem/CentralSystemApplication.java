f8b368d15221e30182deb2f78fca7e99a14c08ef
Dawid <d.kreft@inwebit.pl>
2019-11-12 16:16:50 +0100

    Set basic module

---
+++ central-system/src/main/java/pl/kreft/thesis/ecr/centralsystem/CentralSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.kreft.thesis.ecr.centralsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class CentralSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralSystemApplication.class, args);
+    }
+
+}
---
+++ central-system/src/test/java/pl/kreft/thesis/ecr/centralsystem/CentralSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.kreft.thesis.ecr.centralsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class CentralSystemApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
