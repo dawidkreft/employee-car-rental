352241cfbc6e9b5a1c5012e3d0050bd1933b0b9c
Dawid Kreft <s452117@labs.wmi.amu.edu.pl>
2019-11-17 11:51:43 +0100

    Added db cleaner

--- central-system/src/main/java/pl/kreft/thesis/ecr/centralsystem/car/Starter.java
+++ central-system/src/main/java/pl/kreft/thesis/ecr/centralsystem/car/Starter.java
@@ -17,7 +17,7 @@ public class Starter implements CommandLineRunner {
     @Autowired CarRepository carRepository;
 
     @Override public void run(String... args) throws Exception {
-        carRepository.save(new Car(CarStatus.AVAILABLE,
+        /*carRepository.save(new Car(CarStatus.AVAILABLE,
                 "Tigra",
                 "Opel",
                 CarType.CAR,
@@ -27,6 +27,6 @@ public class Starter implements CommandLineRunner {
                 Instant.now().plus(Duration.ofDays(200)),
                 Instant.now().plus(Duration.ofDays(200)),
                 Instant.now(),
-                false));
+                false));*/
     }
 }
--- central-system/src/test/java/pl/kreft/thesis/ecr/centralsystem/car/repository/CarRepositoryTest.java
+++ central-system/src/test/java/pl/kreft/thesis/ecr/centralsystem/car/repository/CarRepositoryTest.java
@@ -1,5 +1,8 @@
 package pl.kreft.thesis.ecr.centralsystem.car.repository;
 
+import org.assertj.core.api.Fail;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import pl.kreft.thesis.ecr.centralsystem.car.model.Car;
 import pl.kreft.thesis.ecr.centralsystem.testobjectfactories.CarFactory;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pl.kreft.thesis.ecr.centralsystem.dbtestcleaner.DbCleaner.clearDatabase;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,8 +25,17 @@ class CarRepositoryTest {
     @Autowired
     CarRepository carRepository;
 
+    @After
+    public void tearDown() {
+        try {
+            clearDatabase();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError("Initialize db exception");
+        }
+    }
+
     @Test
-    void shouldSaveAndFindById() {
+    void shouldSaveAndFindById() throws SQLException {
         Car testCar = CarFactory.getCar();
         Car testPremiumCar = CarFactory.getPremiumCar();
 
@@ -32,5 +46,17 @@ class CarRepositoryTest {
 
         assertEquals(testCar.getId(), car.get().getId());
         assertEquals(2, allCars.size());
+    }
+
+    @Test
+    void shouldSaveAndFindAllCars() {
+        carRepository.save(CarFactory.getCar());
+        carRepository.save(CarFactory.getCar());
+        carRepository.save(CarFactory.getPremiumCar());
+        carRepository.save(CarFactory.getPremiumCar());
+
+        List<Car> allCars = carRepository.findAll();
+
+        assertEquals(4, allCars.size());
     }
 }
