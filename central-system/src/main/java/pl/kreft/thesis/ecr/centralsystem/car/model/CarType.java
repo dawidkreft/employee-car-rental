64fb4581bd26532d84a088a69bd10f671de91d1a
Dawid <d.kreft@inwebit.pl>
2019-12-16 00:19:14 +0100

    Added date formating in views

--- central-system/src/main/java/pl/kreft/thesis/ecr/centralsystem/car/model/CarType.java
+++ central-system/src/main/java/pl/kreft/thesis/ecr/centralsystem/car/model/CarType.java
@@ -1,8 +1,18 @@
 package pl.kreft.thesis.ecr.centralsystem.car.model;
 
 public enum CarType {
-    CAR,
-    TRACK,
-    VAN,
-    PREMIUM_CAR
+    CAR("Samochód osobowy"),
+    TRACK("Samochód ciężarowy"),
+    VAN("Samochód typu VAN"),
+    PREMIUM_CAR("Samochód premium");
+
+    private final String displayValue;
+
+    private CarType(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
