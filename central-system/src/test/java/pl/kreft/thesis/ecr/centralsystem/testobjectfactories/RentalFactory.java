cd1c35fbf9e7d0faf3c65e505d9f131332eb1230
Dawid <d.kreft@inwebit.pl>
2020-06-28 23:49:34 +0200

    Fix in tests

--- central-system/src/test/java/pl/kreft/thesis/ecr/centralsystem/testobjectfactories/RentalFactory.java
+++ central-system/src/test/java/pl/kreft/thesis/ecr/centralsystem/testobjectfactories/RentalFactory.java
@@ -1,8 +1,8 @@
 package pl.kreft.thesis.ecr.centralsystem.testobjectfactories;
 
 import pl.kreft.thesis.ecr.centralsystem.car.model.Car;
-import pl.kreft.thesis.ecr.centralsystem.rental.model.dto.CarRentalRequest;
 import pl.kreft.thesis.ecr.centralsystem.rental.model.Rental;
+import pl.kreft.thesis.ecr.centralsystem.rental.model.dto.CarRentalRequest;
 import pl.kreft.thesis.ecr.centralsystem.rental.model.dto.ReturnCarRequest;
 import pl.kreft.thesis.ecr.centralsystem.user.model.User;
 
@@ -18,35 +18,35 @@ public class RentalFactory {
 
     static public Rental getRandomRental(User lender, Car car) {
         return Rental.builder()
-                     .car(car)
-                     .lender(lender)
-                     .target("Podróż służbowa")
-                     .carCondition("OK")
-                     .plannedRentalStart(LocalDateTime.now())
-                     .plannedRentalEnd(LocalDateTime.now())
-                     .applicationDate(Instant.now())
-                     .creationDate(Instant.now())
-                     .removed(false)
-                     .build();
+                .car(car)
+                .lender(lender)
+                .target("Podróż służbowa")
+                .carCondition("OK")
+                .plannedRentalStart(LocalDateTime.now())
+                .plannedRentalEnd(LocalDateTime.now())
+                .applicationDate(Instant.now())
+                .creationDate(Instant.now())
+                .removed(false)
+                .build();
     }
 
     static public CarRentalRequest prepareRequest(UUID carId) {
         return CarRentalRequest.builder()
-                               .rentalCarId(carId)
-                               .target("Podróż służbowa")
-                               .dateOfStartRent(LocalDateTime.now())
-                               .dateOfEndRent(LocalDateTime.now())
-                               .build();
+                .rentalCarId(carId)
+                .target("Podróż służbowa")
+                .dateOfStartRent(LocalDateTime.of(2020, 6, 23, 10, 0))
+                .dateOfEndRent(LocalDateTime.of(2020, 6, 23, 15, 0))
+                .build();
     }
 
     static public ReturnCarRequest prepareReturnRequest(UUID rentalId) {
         return ReturnCarRequest.builder()
-                               .rentalId(rentalId)
-                               .carCondition("OK")
-                               .currentFuelLevel(testFuelLevel)
-                               .receivedDescription("OK")
-                               .numberKilometerFromMeter(testCurrentCarKilometer)
-                               .distanceTraveled(testCarDistance)
-                               .build();
+                .rentalId(rentalId)
+                .carCondition("OK")
+                .currentFuelLevel(testFuelLevel)
+                .receivedDescription("OK")
+                .numberKilometerFromMeter(testCurrentCarKilometer)
+                .distanceTraveled(testCarDistance)
+                .build();
     }
 }
